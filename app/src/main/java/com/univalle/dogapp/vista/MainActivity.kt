e70c3f747f7d6d87cecdf38161d4a29b935bc246
DavidRS2354 <david.rodriguez.sanchez@correounivalle.edu.co>
2025-05-08 21:19:00 -0500

    Creation of Dogg for the loggin and the text for de loggin, plus of that creation of the new logo

--- app/src/main/java/com/univalle/dogapp/vista/MainActivity.kt
+++ app/src/main/java/com/univalle/dogapp/vista/MainActivity.kt
@@ -13,9 +13,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
